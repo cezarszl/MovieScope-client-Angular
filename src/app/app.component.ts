7a1e84cf557244eeea47912146a227e45f8e4ba2
cezarszl <cezarszlfedora@gmail.com>
2024-03-03 22:44:48 +0100

    Added welcome-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
+
 
 @Component({
   selector: 'app-root',
@@ -11,19 +13,24 @@ import { MatDialog } from '@angular/material/dialog';
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  constructor(public dialog: MatDialog) { }
+  // constructor(public dialog: MatDialog) { }
 
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-    width: '280px'
-    });
-  }
+  // openUserRegistrationDialog(): void {
+  //   this.dialog.open(UserRegistrationFormComponent, {
+  //   width: '280px'
+  //   });
+  // }
 
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-    width: '280px'
-    });
-  }
+  // openUserLoginDialog(): void {
+  //   this.dialog.open(UserLoginFormComponent, {
+  //   width: '280px'
+  //   });
+  // }
+  // openMoviesDialog(): void {
+  //   this.dialog.open(MovieCardComponent, {
+  //     width: '500px'
+  //   });
+  // }
 }
 
 
