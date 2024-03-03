1e9382805f77dbca50ed081fe149d6ed0701ffb6
cezarszl <cezarszlfedora@gmail.com>
2024-03-03 22:46:42 +0100

    Added user-login component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -6,37 +6,48 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrl: './user-login-form.component.scss'
 })
+
 export class UserLoginFormComponent implements OnInit {
 
- @Input() userData = { Username: '', Password: '' };
-
-constructor(
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+  
+  constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
-
-ngOnInit(): void {  
-}
-
-loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-     this.dialogRef.close(); 
-     console.log(result);
-     this.snackBar.open('Loggin in successful', 'OK', {
-        duration: 2000
-     });
-     localStorage.setItem("user", JSON.stringify(result.user));
-     localStorage.setItem("token", result.token);
-    }, (result) => {
-      console.log(result);
-      this.snackBar.open(result, 'OK', {
-        duration: 2000
-      });
-    });
+    public snackBar: MatSnackBar,
+    public router: Router) { }
+  
+    
+  ngOnInit(): void {
+  }
+  
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe({
+      next: (result) => {
+        this.dialogRef.close();
+        console.log(result);
+        this.snackBar.open('Loggin in successful', 'OK', {
+          duration: 2000
+        });
+        localStorage.setItem("userData", JSON.stringify(result.user));
+        localStorage.setItem("token", result.token);
+        this.router.navigate(['movies']);
+      },
+      error: (result) => {
+        console.log(result);
+        this.snackBar.open(result, 'OK', {
+          duration: 2000
+        });
+      }
+    })
   }
 }
+
